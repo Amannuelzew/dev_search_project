f06fe81336e218ebc4c9f891f7c2c2c9a2995a83
amanzew <amanzew@gmail.com>
2024-04-08 11:42:51 +0300

    create profile

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . import views
-
+from projects import views
+from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', views.index,name="index"),
+    path('projects', views.index,name="projects"),
+   path('profile/', login_required(views.ProfileView.as_view()),name="profile"),
+
+    path('signup/', views.signup_page,name="signup"),
     path('login/', views.login_page,name="login"),
     path('logout/', views.logout_page,name="logout"),
    
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
 from datetime import date
+from django.urls import reverse
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
+from .forms import ProfileForm
+from .models import Profiles
 from django.contrib import messages
 # Create your views here.
 projects=[
@@ -31,11 +37,10 @@ def login_page(request):
      username=request.POST.get("username")
      password=request.POST.get("password")
      user=authenticate(request,username=username,password=password)
-     if user is not None:
+     if user :
         login(request,user)
         return redirect("index")
      else:
-        print("yes")
         messages.error(request,"does not match")
 
   return render(request,"projects/login.html")
@@ -44,11 +49,41 @@ def logout_page(request):
    logout(request)
    return redirect("login")
 
+def signup_page(request):
+   if request.method=="POST":
+      form=UserCreationForm(request.POST)
+      if form.is_valid():
+         user=form.save(commit=False)
+         user.username.lower()
+         user.save()
+         messages.success(request,"account created")
+         login(request,user)
+         return redirect("index")
+      else:
+         messages.success(request,"error")
+   form=UserCreationForm()
+   return render(request,"projects/signup.html",{"forms":form})
+   
+
+class ProfileView(FormView):
+   template_name="projects/profile.html"
+   form_class=ProfileForm
+   success_url="/"
+   redirect_authenticated_user=True
+
+   def form_valid(self, form):
+      user=form.save(commit=False)
+      user.name.lower()
+      user.save()
+      return super().form_valid(form)
+
+@login_required(login_url="login")
 def index(request):
+    developers=Profiles.objects.all()
     if request.GET.get("search_query"):
         query=request.GET.get("search_query")
         dev=[x for x in developers if query.lower() in  x["username"].lower()]
-        print(len(dev) )
+      
         context={"developers":dev}
         return render(request,"projects/index.html",context)
     
