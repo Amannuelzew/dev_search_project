26ffaf7f9fa6f82768e0027e38ed8c79e53d7b8c
amanzew <amanzew@gmail.com>
2024-04-08 09:12:33 +0300

    pagination added

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from datetime import date
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+
 # Create your views here.
 projects=[
     {"title":"hello world","description":"hello","demo_link":"https://a.com",
@@ -11,15 +13,33 @@ developers=[
                   animi, exercitationem molestias nam doloremque architecto odit.""","position":"FullStack Web Designer & Developer",
      "tags":["python","react","web component","vue"]},
      {"username":"my name world","description":"name","position":"back end web",
+     "tags":["c#","c++","c","GraphQL"]},
+     {"username":"sol","description":"name","position":"back end web",
+     "tags":["c#","c++","c","GraphQL"]},
+     {"username":"don","description":"name","position":"back end web",
+     "tags":["c#","c++","c","GraphQL"]},
+     {"username":"joel","description":"name","position":"back end web",
+     "tags":["c#","c++","c","GraphQL"]},
+     {"username":"sam","description":"name","position":"back end web",
      "tags":["c#","c++","c","GraphQL"]}
 ]
 def index(request):
     if request.GET.get("search_query"):
         query=request.GET.get("search_query")
-        dev=[x for x in developers if x["username"].lower()==query]
+        dev=[x for x in developers if query.lower() in  x["username"].lower()]
         print(len(dev) )
         context={"developers":dev}
         return render(request,"projects/index.html",context)
     
-    context={"developers":developers}
+    page=request.GET.get("page")
+    p=Paginator(developers,3)
+    try:
+       dev=p.page(page)
+    except PageNotAnInteger:
+       dev=p.page(1)
+    except EmptyPage:
+       dev=p.page(p.num_pages)
+    
+    print(dev)
+    context={"developers":dev,"Paginator":p}
     return render(request,"projects/index.html",context)
